c7aecd1000b3f4e4ea2dda93566cdc9058532cc6
sartechb <sarthak1657@gmail.com>
2016-07-29 20:17:19 -0400

    added base search

--- classes/search.py
+++ classes/search.py
@@ -1,5 +1,7 @@
 import requests
 import json
+from address import Address
+from pprint import pprint
 
 headers = {'X-Access-Token': 'b6832dabf2b76e48'}
 
@@ -7,25 +9,28 @@ def search(query):
 	'''
 	searches and passes back list of restaurants with query applied
 	'''
-	pass
-
+	#grab address (TODO: grab from saved address)
+	address = '131+West+Wilson+St.+Madison,+WI'
+	requestUrl = 'https://api.eatstreet.com/publicapi/v1/restaurant/search?method=both&search=' + query + '&street-address=' + address
+	response = requests.get(requestUrl, headers=headers)
+	restaurants = json.loads(response.content)['restaurants']
+	# pprint(restaurants)
+	return restaurants
+	
 def searchByName(name):
 	'''
 	filters list by name
 	'''
-	search(name)
-	pass
+	restaurants = search(name)
 
 def searchByCategory(category):
 	'''
 	filters list by category
 	'''
-	search(category)
-	pass
+	restaurants = search(category)
 
 def searchByItem(item):
 	'''
 	filters list by item
 	'''
-	search(item)
-	pass
+	restaurants = search(item)
